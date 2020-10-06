74f28b3c16dcbffd6c91abc50f1d2ebd1c94cc82
Louis Sommer <louis.sommer@gmail.com>
2020-10-06 17:02:42 +0200

    writing specsc

--- spec/system/nested_attributes_spec.rb
+++ spec/system/nested_attributes_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe "Nested attributes behaviour" , type: :system do
       expect(Project.last.title).to eq("A nice project")
     end
 
+    it "creates a project along with a few tasks" do
+      visit new_project_path
+      fill_in('project_title', with: "A project with two tasks")
+      fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")
+      click_on('Add Task')
+      within("div.project_tasks_title[data-children-count="1"]") do
+        fill_in('')
+
+      end
+      click_on('Save')
+      expect(Project.last.title).to eq("A nice project")
+    end
+  end
+
+  context "Updating an existing project", js: true do
     it 'updates a project without any tasks' do
       project = create(:project)
       visit edit_project_path(project)
@@ -18,6 +33,5 @@ RSpec.describe "Nested attributes behaviour" , type: :system do
       project.reload
       expect(project.title).to eq('A rather small project')
     end
-    
   end
 end
