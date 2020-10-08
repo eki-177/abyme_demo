80e1d24361c39fd3727754528438a78f0db4ec84
Louis Sommer <louis.sommer@gmail.com>
2020-10-08 12:38:38 +0200

    setup modified, tests passing for update

--- spec/support/helpers/add_nested_attributes.rb
+++ spec/support/helpers/add_nested_attributes.rb
@@ -1,19 +1,19 @@
-# def add_tasks
-#   2.times { find("div", id: "add-task").click }
-#   titles = find_all_by_id('input', /project_tasks_attributes_\d*_title/)
-#   descriptions = find_all_by_id('textarea', /project_tasks_attributes_\d*_description/ )
-#   titles.each_with_index {|title, n| title.fill_in(with: "Task #{n + 1}") }
-#   descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
-# end
+def add_tasks(number = 2)
+  number.times { find("div", id: "add-task").click }
+  titles = find_all_by_id('input', /project_tasks_attributes_\d*_title/)
+  descriptions = find_all_by_id('textarea', /project_tasks_attributes_\d*_description/ )
+  titles.each_with_index {|title, n| title.fill_in(with: "Task #{n + 1}") }
+  descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
+end
 
-# def fill_project_fields(title)
-#   fill_in('project_title', with: title)
-#   fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")
-# end
+def fill_project_fields(title)
+  fill_in('project_title', with: title)
+  fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")
+end
 
-# def add_comments
-#   add_comment_buttons = all('div', id: "add-comment")
-#   add_comment_buttons.each {|b| 2.times { b.click } }
-#   comment_contents = find_all_by_id('input', /content/)
-#   comment_contents.each_with_index {|c, n| c.fill_in(with: "Comment ##{n}") }
-# end
+def add_comments(number = 2)
+  add_comment_buttons = all('div', id: "add-comment")
+  add_comment_buttons.each {|b| number.times { b.click } }
+  comment_contents = find_all_by_id('input', /content/)
+  comment_contents.each_with_index {|c, n| c.fill_in(with: "Comment ##{n}") }
+end
--- spec/system/nested_attributes_spec.rb
+++ spec/system/nested_attributes_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Nested attributes behaviour", type: :system do
+  let(:description) { "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction)." }
   context "Creating a brand new project" do
     it 'creates a project without any tasks' do
       visit new_project_path
       fill_in('project_title', with: "A project with no task")
-      fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")    
+      fill_in('project_description', with: description)    
       click_on('Save')
       expect(Project.last.title).to eq("A project with no task")
     end
@@ -13,12 +14,8 @@ RSpec.describe "Nested attributes behaviour", type: :system do
     it "creates a project along with a few tasks", js: true do
       visit new_project_path
       fill_in('project_title', with: "A project with two tasks")
-      fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")    
-      2.times { find("div", id: "add-task").click }
-      titles = find_all_by_id('input', /project_tasks_attributes_\d*_title/)
-      descriptions = find_all_by_id('textarea', /project_tasks_attributes_\d*_description/ )
-      titles.each_with_index {|title, n| title.fill_in(with: "Task #{n + 1}") }
-      descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
+      fill_in('project_description', with: description)    
+      add_tasks
       click_on('Save')
       expect(Project.last.title).to eq('A project with two tasks')
       expect(Project.last.tasks.count).to eq(2)
@@ -27,25 +24,18 @@ RSpec.describe "Nested attributes behaviour", type: :system do
     it "creates a project along with a few tasks, each with a few comments", js: true do
       visit new_project_path
       fill_in('project_title', with: "Another project with two tasks")
-      fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")    
-      2.times { find("div", id: "add-task").click }
-      titles = find_all_by_id('input', /project_tasks_attributes_\d*_title/)
-      descriptions = find_all_by_id('textarea', /project_tasks_attributes_\d*_description/ )
-      titles.each_with_index {|title, n| title.fill_in(with: "Task #{n + 1}") }
-      descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
-      add_comment_buttons = all('div', id: "add-comment")
-      add_comment_buttons.each {|b| 2.times { b.click } }
-      comment_contents = find_all_by_id('input', /content/)
-      comment_contents.each_with_index {|c, n| c.fill_in(with: "Comment ##{n}") }
+      fill_in('project_description', with: description)    
+      add_tasks
+      add_comments
       click_on('Save')
       expect(Project.last.comments.count).to eq(4)
     end
   end
 
   context "Updating an existing project" do
-    before(:all) { @project = create(:project) }
+    before(:each) { @project = create(:project) }
 
-    it 'updates a project without any tasks' do
+    it 'updates a project without any tasks', js: true do
       visit edit_project_path(@project)
       fill_in('project_title', with: "A rather small project")
       click_on('Save')
@@ -55,10 +45,12 @@ RSpec.describe "Nested attributes behaviour", type: :system do
 
     it 'updates a project by adding a few tasks', js: true do
       visit edit_project_path(@project)
-      fill_in('project_title', with: "A rather small project")
+      add_tasks(3)
+      add_comments(3)
       click_on('Save')
       @project.reload
-      expect(@project.title).to eq('A rather small project')
+      expect(@project.tasks.count).to eq(3)
+      expect(@project.comments.count).to eq(9)
     end
 
   end
