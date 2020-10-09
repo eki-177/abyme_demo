dd02c54385e9e34123438e5fd3885f3f4f5791af
romain sanson <romainsanson.gm@gmail.com>
2020-10-09 23:06:00 +0200

    abymize default wip

--- app/javascript/controllers/abyme_controller.js
+++ app/javascript/controllers/abyme_controller.js
@@ -22,7 +22,7 @@ export default class extends Controller {
     if (html.match(/<template[\s\S]+<\/template>/)) {
       const template = html
         .match(/<template[\s\S]+<\/template>/)[0]
-        .replace(/(\[\d+\])(\[[^\[\]]+\]"){1}/g, `[NEW_RECORD]$2`);
+        .replace(/(\[\d{12,}\])(\[[^\[\]]+\]"){1}/g, `[NEW_RECORD]$2`);
 
       html = html.replace(/<template[\s\S]+<\/template>/g, template);
     }
