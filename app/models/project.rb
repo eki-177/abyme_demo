31f8452f0f5ce09278c2d4b81645e7ac57d7d8d0
romain sanson <romainsanson.gm@gmail.com>
2020-10-11 03:10:17 +0200

    tests ok

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   
   has_many :tasks, inverse_of: :project, dependent: :destroy
   has_many :comments, through: :tasks
-  has_many :participants
+  has_many :participants, dependent: :destroy
   
   abyme_for :tasks
   abyme_for :participants
--- lib/abyme/view_helpers.rb
+++ lib/abyme/view_helpers.rb
@@ -3,27 +3,25 @@ module Abyme
     def abymize(association, form, options = {}, &block)
       content_tag(:div, data: { controller: 'abyme' }) do
         if block_given?
-          @association = association
-          @form = form
           yield(self)
         else
           model = association.to_s.singularize.classify.constantize
-          concat(abyme_records(association, form, options))
-          concat(abyme_for(model.new, form, options)) 
-          concat(abyme_add_association(content: options[:add_button] || "Add #{model}"))
+          concat(records(association, form, options))
+          concat(new_record(model.new, form, options)) 
+          concat(add_association(content: "Add #{model}"))
         end
       end
     end
 
-    def new_record(options = {}, &block)
-      content_tag(:div, data: { target: 'abyme.associations', model: @association, abyme_position: options[:position] || :end }) do
-        content_tag(:template, class: "abyme--#{@association.to_s.singularize}_template", data: { target: 'abyme.template' }) do
-          @form.fields_for @association, @association.to_s.singularize.classify.constantize.new, child_index: 'NEW_RECORD' do |f|
+    def new_record(association, form, options = {}, &block)
+      content_tag(:div, data: { target: 'abyme.associations', model: formatize(association), abyme_position: options[:position] || :end }) do
+        content_tag(:template, class: "abyme--#{formatize(association).singularize}_template", data: { target: 'abyme.template' }) do
+          form.fields_for formatize(association), association, child_index: 'NEW_RECORD' do |f|
             content_tag(:div, class: 'abyme--fields') do
               if options[:partial]
                 render(options[:partial], f: f)
               else
-                render("#{@association.to_s.singularize}_fields", f: f)
+                render("#{formatize(association).singularize}_fields", f: f)
               end
             end
           end
@@ -31,30 +29,30 @@ module Abyme
       end
     end
   
-    def records(options = {})
+    def records(association, form, options = {})
       if options[:collection]
         records = options[:collection]
       else
-        records = @form.object.send(@association)
+        records = form.object.send(association)
       end
   
       if options[:order].present?
         records = records.order(options[:order])
   
         # GET INVALID RECORDS
-        invalids = @form.object.send(@association).reject(&:persisted?)
+        invalids = form.object.send(association).reject(&:persisted?)
       
         if invalids.any?
           records = records.to_a.concat(invalids)
         end
       end
   
-      @form.fields_for @association, records do |f|
+      form.fields_for association, records do |f|
         content_tag(:div, class: 'abyme--fields') do
           if options[:partial]
             render(options[:partial], f: f)
           else
-            render("#{@association.to_s.singularize}_fields", f: f)
+            render("#{association.to_s.singularize}_fields", f: f)
           end
         end
       end
@@ -65,7 +63,7 @@ module Abyme
       create_button(action, options, &block)
     end
   
-    def abyme_remove_association(options = {}, &block)
+    def remove_association(options = {}, &block)
       action = 'click->abyme#remove_association'
       create_button(action, options, &block)
     end
@@ -75,7 +73,7 @@ module Abyme
     def create_button(action, options, &block)
       options[:attributes] = {} if options[:attributes].nil?
       options[:tag] = :button if options[:tag].nil?
-  
+
       if block_given?
         content_tag(options[:tag], {data: { action: action }}.merge(options[:attributes])) do
           capture(&block)
@@ -89,23 +87,4 @@ module Abyme
       association.class.name.tableize
     end
   end
-
-  class AbymeBuilder
-    # include Abyme::ViewHelpers
-
-    # attr_accessor :association, :form
-
-    # def abyme_for(options = {})
-
-    # end
-
-    # def formatize(association)
-    #   association.class.name.tableize
-    # end
-  end
-
 end
-
-# class ActionView::Helpers::FormBuilder
-#   include Abyme::ViewHelpers
-# end
