d3e45b94cbac2a7683c318d9b6e4a82ce92e03bd
romain sanson <romainsanson.gm@gmail.com>
2020-10-12 12:27:03 +0200

    add specs

--- spec/support/helpers/add_nested_attributes.rb
+++ spec/support/helpers/add_nested_attributes.rb
@@ -6,6 +6,13 @@ def add_tasks(number = 2)
   descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
 end
 
+def add_tasks_with_errors(number = 2)
+  number.times { find("div", id: "add-task").click }
+  titles = find_all_by_id('input', /project_tasks_attributes_\d*_title/)
+  descriptions = find_all_by_id('textarea', /project_tasks_attributes_\d*_description/ )
+  descriptions.each_with_index {|title, n| title.fill_in(with: "Small description for task number #{n + 1}") }
+end
+
 def fill_project_fields(title)
   fill_in('project_title', with: title)
   fill_in('project_description', with: "La mise en abyme — également orthographiée mise en abysme ou plus rarement mise en abîme1 — est un procédé consistant à représenter une œuvre dans une œuvre similaire, par exemple dans les phénomènes de « film dans un film », ou encore en incrustant dans une image cette image elle-même (en réduction).")
@@ -19,8 +26,8 @@ def add_comments(number = 2)
 end
 
 def add_participants(number = 2)
-  number.times { click_on("Add Participant") }
-  within('div[data-model="participants"]') do
+  number.times { click_on('add participant') }
+  within('div[data-association="participants"]') do
     emails = all("input")
     emails.each_with_index {|input, index| input.fill_in(with: "email_#{index}@gmail.com") }
   end
