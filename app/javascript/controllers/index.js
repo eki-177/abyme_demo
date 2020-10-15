444b9ca6d9ac0640661a7bc271db54dd017fba1b
Louis Sommer <louis.sommer@gmail.com>
2020-10-15 23:01:15 +0200

    add abyme as gem

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,10 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { AbymeController } from 'abyme'
+
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+application.register('abyme', AbymeController)
